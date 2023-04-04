756eddf1799d40a674cb76bf47c2f9555c946873
yeyel <arielglvz@gmail.com>
2023-04-04 18:25:44 +0800

    day 2

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,18 +1,28 @@
 import { useNavigate } from "react-router-dom";
 import { AiOutlineArrowLeft } from "react-icons/ai";
+import { BsFacebook } from "react-icons/bs";
+import { FcGoogle } from "react-icons/fc";
+// import { FiEye, FiEyeOff } from "react-icons/fi";
 import './styles.scss';
+import { IconContext } from "react-icons";
 
 const Login = () => {
     const navigate = useNavigate()
+
+    const backBtnHandler = () => {
+        navigate('/')
+    }
 
     return(
         <div className="Login">
             {/* Study Reusable Component for reusable button
             Study Material UI 
             Study Vue 2*/}
+            {/* json base web server for beginner  */}
+            {/* JWT auth */}
             
             <div className="login-container">
-                <button className="back-btn">
+                <button className="back-btn" onClick={backBtnHandler}>
                     <span>
                         <AiOutlineArrowLeft />
                     </span>
@@ -23,22 +33,35 @@ const Login = () => {
                         <label htmlFor="">E-mail</label>
                         <input type="email" name="email" id="email" placeholder="Your email or phone"/>
                     </div>
-                    <div className="form-item">
+                    <div className="form-item password">
                         <label htmlFor="">Password</label>
                         <input type="password" name="password" id="password" placeholder="Password"/>
+                        {/* {true ? <FiEye /> :<FiEyeOff />} */}
                     </div>
                     <div className="btn-container">
                         <p className="forgot-password">Forgot Password?</p>
                         <button className="login-btn">login</button>
-                        <p className="sign-up">Don't have an account? <span>Sign Up</span></p>
+                        <p className="sign-up">Don't have an account? <span onClick={()=>{navigate('/signup')}}>Sign Up</span></p>
                     </div>
                 </form>
                 <form className="sign-in-with">
                     <fieldset>
                     <legend>Sign in with</legend>
                         <div className="social-signin">
-                            <div className="social-item">Facebook</div>
-                            <div className="social-item">Google</div>
+                            <div className="social-item">
+                               <IconContext.Provider
+                                value={{size: '38px', color: '#1877F2'}}>
+                                <BsFacebook />
+                                </IconContext.Provider>
+                                <p>FACEBOOK</p>
+                            </div>
+                            <div className="social-item">
+                                <IconContext.Provider
+                                    value={{size: '38px'}}>
+                                    <FcGoogle />
+                                </IconContext.Provider>
+                                    <p>GOOGLE</p>
+                            </div>
                         </div>
                     </fieldset>
                 </form>
--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,8 +1,58 @@
+import { AiOutlineArrowLeft } from "react-icons/ai";
+import { BsFacebook } from "react-icons/bs";
+import { FcGoogle } from "react-icons/fc"
 import './styles.scss';
+import { IconContext } from "react-icons";
+import { useNavigate } from "react-router-dom";
 
 const SignUp = () => {
+    const navigate = useNavigate();
+
     return(
-        <h1>Sign Up</h1>
+       <div className="SignUp">
+            <div className="signup-container">
+                <form className="signup-form">
+                    <h1>Sign Up</h1>
+                    <div className="form-item">
+                        <label htmlFor="">Full name</label>
+                        <input type="text" name="fullname" id="fullname" placeholder="Your full name"/>
+                    </div>
+                    <div className="form-item">
+                        <label htmlFor="">E-mail</label>
+                        <input type="email" name="email" id="email" placeholder="Your email or phone number"/>
+                    </div>
+                    <div className="form-item">
+                        <label htmlFor="">Password</label>
+                        <input type="password" name="password" id="password" placeholder="Password"/>
+                    </div>
+                    <div className="btn-container">
+                            <button className="signup-btn">SIGN UP</button>
+                            <p className="login">Already have an account? <span onClick={()=>{navigate('/login')}}>Login</span></p>
+                        </div>
+                </form>
+                <form className="sign-up-with">
+                        <fieldset>
+                        <legend>Sign up with</legend>
+                            <div className="social-signup">
+                                <div className="social-item">
+                                <IconContext.Provider
+                                    value={{size: '38px', color: '#1877F2'}}>
+                                    <BsFacebook />
+                                    </IconContext.Provider>
+                                    <p>FACEBOOK</p>
+                                </div>
+                                <div className="social-item">
+                                    <IconContext.Provider
+                                        value={{size: '38px'}}>
+                                        <FcGoogle />
+                                    </IconContext.Provider>
+                                        <p>GOOGLE</p>
+                                </div>
+                            </div>
+                        </fieldset>
+                </form>
+            </div>
+       </div>
     )
 }
 
