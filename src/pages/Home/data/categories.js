8f8f6f16150b1715b4c68868223b3c4df4702437
yeyel <arielglvz@gmail.com>
2023-04-05 17:32:42 +0800

    day 3

--- src/pages/Home/data/categories.js
+++ src/pages/Home/data/categories.js
@@ -1,40 +1,40 @@
-import Burger from '../../';
-import Donut from '../../../assets/home/category/donut.png';
-import Pizza from '../../../assets/home/category/pizza.png';
-import Sandwich from '../../../assets/home/category/sandwich.png';
-import Bread from '../../../assets/home/category/bread.png';
-import Dessert from '../../../assets/home/category/dessert.png';
+import Burger from '../../../assets/home/category/burger@2x.png';
+import Donut from '../../../assets/home/category/donut@2x.png';
+import Pizza from '../../../assets/home/category/pizza@2x.png';
+import Sandwich from '../../../assets/home/category/sandwich@2x.png';
+import Bread from '../../../assets/home/category/bread@2px.png';
+import Dessert from '../../../assets/home/category/dessert@2x.png';
 
 const Category = [
     {
-        id : 1,
-        img : Burger,
-        name: 'Burger'
+        'id' : 1,
+        'img' : Burger,
+        'name': 'Burger'
     },
     {
-        id : 2,
-        img : Donut,
-        name: 'Donut'
+        'id' : 2,
+        'img' : Donut,
+        'name': 'Donut'
     },
     {
-        id : 3,
-        img : Pizza,
-        name: 'Pizza'
+        'id' : 3,
+        'img' : Pizza,
+        'name': 'Pizza'
     },
     {
-        id : 4,
-        img : Sandwich,
-        name: 'Sandwich'
+        'id' : 4,
+        'img' : Sandwich,
+        'name': 'Sandwich'
     },
     {
-        id : 5,
-        img : Bread,
-        name: 'Bread'
+        'id' : 5,
+        'img' : Bread,
+        'name': 'Bread'
     },
     {
-        id : 6,
-        img : Dessert,
-        name: 'Dessert'
+        'id' : 6,
+        'img' : Dessert,
+        'name': 'Dessert'
     }
 ]
 
