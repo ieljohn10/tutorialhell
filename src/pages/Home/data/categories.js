7009bf043c3e4ed320c993f9afcd4ad72457b065
yeyel <arielglvz@gmail.com>
2023-04-05 15:59:32 +0800

    day3

---
+++ src/pages/Home/data/categories.js
@@ -0,0 +1,41 @@
+import Burger from '../../';
+import Donut from '../../../assets/home/category/donut.png';
+import Pizza from '../../../assets/home/category/pizza.png';
+import Sandwich from '../../../assets/home/category/sandwich.png';
+import Bread from '../../../assets/home/category/bread.png';
+import Dessert from '../../../assets/home/category/dessert.png';
+
+const Category = [
+    {
+        id : 1,
+        img : Burger,
+        name: 'Burger'
+    },
+    {
+        id : 2,
+        img : Donut,
+        name: 'Donut'
+    },
+    {
+        id : 3,
+        img : Pizza,
+        name: 'Pizza'
+    },
+    {
+        id : 4,
+        img : Sandwich,
+        name: 'Sandwich'
+    },
+    {
+        id : 5,
+        img : Bread,
+        name: 'Bread'
+    },
+    {
+        id : 6,
+        img : Dessert,
+        name: 'Dessert'
+    }
+]
+
+export default Category;
